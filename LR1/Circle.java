cc1a7d66134cc4151abba1dbdecf6498731e814f
Alexander Tinkovan <mascot_91@mail.ru>
2016-08-25 01:54:33 +0300

    LR1 finished 4.6.3

--- LR1/Circle.java
+++ LR1/Circle.java
@@ -1,29 +1,70 @@
 
 import java.io.*;
+import java.util.Scanner;
 class Circle{
-		private double radius, square, circuit;
-		private String s1, s2;
+		private double radius, square, perimeter, height, base;
+		private String s1;
+		private static int chooice;
 		
-		private void processIt() throws IOException {
+		private void choose(){
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Choose figure whose square and perimeter will be calculated: " + "\t1. Triangle" + "\t2. Quadrate" + "\t3. Circle");
+			chooice = sc.nextInt();
+		}
+		
+		private void processTriangle(){
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter the height of the triangle: ");
+			height = sc.nextInt();
+			System.out.println("Enter the base of the triangle: ");
+			base = sc.nextInt();
+			square = 0.5 * base * height;
+			perimeter = 3 * base;
+				System.out.println("Your Triangle with: " + "\tHeight = " + height + "\tBase = " + base);
+				System.out.println("Square = " + square + "\tPerimeter = " + perimeter);
+		}
+		
+		private void processQuadrate(){
+			Scanner sc = new Scanner (System.in);
+			System.out.println("Enter the base of the quadrate: ");
+			base = sc.nextInt();
+			square = base * base;//Math.pow(base,2);
+			perimeter = 4 * base;
+				System.out.println("Your Quadrate with: " + "\tBase = " + base);
+				System.out.println("Square = " + square + "\tPerimeter = " + perimeter);
+		}
+		
+		private void processCircle() throws IOException {
 			//Создадим стандартный поток ввода
 			BufferedReader stdin = new BufferedReader(new InputStreamReader (System.in));
-			System.out.println("Enter radius of the circle: ");
+			System.out.println("Enter the radius of the circle: ");
 			s1 = stdin.readLine(); //Введем текст в поле message
 			//Преобразуем строку в число и выполним вычисления.
 			//Предусмотрим обработку ошибок преобразования сроки в число.
 			try {
 				radius = Double.parseDouble(s1);
 				
-				circuit = 2 * Math.PI * radius;
+				perimeter = 2 * Math.PI * radius;
 				square = Math.PI * Math.pow(radius, 2);
-				System.out.println("R = " + radius + "\tS = " + square + "\tL = " + circuit);
+				System.out.println("Your Circle with: " + "\tRadius = " + radius);
+				System.out.println("Square = " + square + "\tPerimeter = " + perimeter);
 			} catch (Exception ex){
 				System.out.println("Could not convert the text "+ s1 +" in the number!");
 			}			
 		}
+		
+		
 		public static void main (String[] args) throws IOException { 
 			Circle c = new Circle(); //Создаем екземпляр класса Circle
-			c.processIt(); //Вызовем метод processIt() екземпляра класса с
+			c.choose();
+			if(chooice == 1){
+				c.processTriangle();
+			}if(chooice == 2){
+				c.processQuadrate();
+			}else if (chooice == 3){
+			c.processCircle();	//Вызовем метод processIt() екземпляра класса с	
+			}
+			
 		}
 }
 
