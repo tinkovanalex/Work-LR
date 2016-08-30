3fe8f445ae8110bddcc21a65c51f14c866b107c9
Alexander Tinkovan <mascot_91@mail.ru>
2016-08-30 23:40:56 +0300

    LR1 finished everything

--- LR1/HelloWord.java
+++ LR1/HelloWord.java
@@ -1,26 +1,34 @@
 import java.util.Scanner;
 public class HelloWord{
-public static void main (String [] args){
-	Scanner sc = new Scanner(System.in);
-		System.out.println("Enter Rectangle length!");
-		int length = sc.nextInt();
-		System.out.println("Enter Rectangle wedth!");
-		int wedth = sc.nextInt();	
-	for (int i = 0; i < wedth; i++ ){
-		for(int j = 0; j < length; j++){
+	
+		private String rectangleLength = "Enter Rectangle length!";
+		private String rectangleWidth = "Enter Rectangle width";
+		private String enterMessage = "Enter Triangle length of the triangle base!";
+		
+	
+		private void rectangleCreation (){
+			Scanner sc = new Scanner(System.in);
+			System.out.println(rectangleLength);
+			int length = sc.nextInt();
+			System.out.println(rectangleWidth);
+		    int wedth = sc.nextInt();	
+			for (int i = 0; i < wedth; i++ ){
+				for(int j = 0; j < length; j++){
 				System.out.print("*");
 			}	
 			System.out.println("");
 		}
+	}
 	
-	System.out.println("Enter Trianle length of the Trianle base!");
-	int lengthOfBase = sc.nextInt();
 	
-	int spaces = lengthOfBase -1 ;
-	String stars = "*";
-	
-	for(int i = 0; i < lengthOfBase; i++){
-		for(int j = 0; j < spaces; j++){
+		private void triangleCreation(){
+			Scanner sc = new Scanner(System.in);
+			System.out.println(enterMessage);
+			int lengthOfBase = sc.nextInt();	
+			int spaces = lengthOfBase -1 ;
+			String stars = "*";
+			for(int i = 0; i < lengthOfBase; i++){
+				for(int j = 0; j < spaces; j++){
 			System.out.print(" ");
 		}
 			System.out.print(stars);
@@ -29,4 +37,9 @@ public static void main (String [] args){
 			System.out.println("");
 		}
 	}
+		public static void main (String [] args){
+			HelloWord hw = new HelloWord();
+			hw.rectangleCreation();
+			hw.triangleCreation();
+		}
 }
